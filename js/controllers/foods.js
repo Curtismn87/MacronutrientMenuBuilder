a67fe418a7aaa6eed6214b720525acee78a22923
Matt Curtis <Curtismn87@yahoo.com>
2015-09-14 19:58:36 -0400

    added CRUD

--- js/controllers/foods.js
+++ js/controllers/foods.js
@@ -2,10 +2,25 @@
   var app = angular.module("MenuBuilder");
   app.controller("foodsController", function(){
     this.foods = [
-      "Chicken",
-      "Beef",
-      "Pizza",
-      "Cake"
+      {name: "Chicken"}, {name: "Beef"}, {name: "Pizza"}, {name: "Cake"}
     ];
-  });
+    this.create = function(){
+      this.foods.unshift({
+        name: this.food,
+      });
+      this.reset();
+    }; // end of create function
+    this.reset = function(){
+          this.food = "";
+        }; // end reset function
+    this.edit = function(index){
+      var food = this.foods[index];
+      food.name = this.name;
+    };
+    this.delete = function(index){
+      this.foods.splice(index, 1);
+    }
+
+
+  }); // end of controller
 })();
