ad7f258d0244a56e9edd4fc8bd9d2704e7776531
Matt Curtis <Curtismn87@yahoo.com>
2015-09-15 16:11:55 -0400

    trying to get API call to work

--- js/controllers/foods.js
+++ js/controllers/foods.js
@@ -1,27 +1,52 @@
 (function() {
   var app = angular.module("MenuBuilder", []);
 
-  app.controller("foodsController", function(){
-    this.foods = [
-      {name: "Chicken"}, {name: "Beef"}, {name: "Pizza"}, {name: "Cake"}
-    ];
-    this.create = function(){
-      this.foods.unshift({
-        name: this.food,
-      });
-      this.reset();
-    }; // end of create function
-    this.reset = function(){
-          this.food = "";
-        }; // end reset function
-    this.edit = function(index){
-      var food = this.foods[index];
-      food.name = this.name;
-    };
-    this.delete = function(index){
-      this.foods.splice(index, 1);
-    };
-
-
-  }); // end of controller
+  app.controller("foodsController",["$scope", "$http", function(){
+    console.log("before getFoods is run");
+      this.getFoods = function($scope, $http){
+        console.log("button clicked");
+        $http.get("http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=M4zdaQiev4SOfYzye5gC3xhVTanoFD4uKXt1TNe8&nutrients=205&nutrients=204&nutrients=208&nutrients=269")
+        .success(function(response){
+          $scope.food = response;
+          console.log(response);
+        })
+        .fail(function(){
+          console.log("call has failed");
+        });
+      };
+  }]);
 })();
+
+
+
+// app.controller("foodsController", function(){
+//   console.log("before getFoods is run");
+//     this.getFoods = function (){
+//       console.log("button clicked");
+//       $http.get("http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=M4zdaQiev4SOfYzye5gC3xhVTanoFD4uKXt1TNe8&nutrients=205&nutrients=204&nutrients=208&nutrients=269")
+//       .success(function(response){
+//         console.log(response);
+//       })
+//       .fail(function(){
+//         console.log("call has failed");
+//       });
+//     };
+// }); // end of controller
+
+// this.create = function(){
+//   this.foods.unshift({
+//     name: this.food,
+//   });
+//   this.reset();
+// }; // end of create function
+// this.reset = function(){
+//       this.food = "";
+//     }; // end reset function
+// this.edit = function(index){
+//   var food = this.foods[index];
+//   food.name = this.name;
+// };
+// this.delete = function(index){
+//   this.foods.splice(index, 1);
+// };
+//
