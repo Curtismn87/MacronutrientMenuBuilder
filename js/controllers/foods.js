9ef09987b43576e723dcb680f79ea03c257b7183
Matt Curtis <Curtismn87@yahoo.com>
2015-09-16 18:20:02 -0400

    added some bootstrap and a couple other things

--- js/controllers/foods.js
+++ js/controllers/foods.js
@@ -3,38 +3,36 @@
 
   app.controller("foodsController",["$scope", "$http" , function($scope, $http){
     console.log("before getFoods is run");
+    $scope.jsonfoods = {};
     this.getFoods = function(){
         console.log("button clicked");
-        $http.get("http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=M4zdaQiev4SOfYzye5gC3xhVTanoFD4uKXt1TNe8&nutrients=205&nutrients=204&nutrients=203")
+        $http.get("http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=M4zdaQiev4SOfYzye5gC3xhVTanoFD4uKXt1TNe8&max=15&fg=13&nutrients=205&nutrients=204&nutrients=203")
         .success(function(response){
           $scope.jsonfoods = response.report.foods;
-
           console.log(response.report.foods);
-          console.log(response.report.foods[0].nutrients[0].nutrient);
-          console.log(response.report.foods[0].nutrients[0].value);
         });
       };
       this.delete = function(index){
-        jsonfoods.delete(index, 1);
+        $scope.jsonfoods.splice(index, 1);
       };
+
+    var self = this;
+    self.showProtein = true;
+    self.toggleShowProtein = function(value){
+      var input = this.protein;
+      console.log($scope.jsonfoods);
+      console.log(input);
+      for (var i=0; i < $scope.jsonfoods.length; i ++){
+        for (var x=0; x < $scope.jsonfoods.nutrients.length; x++){
+          if (input > $scope.jsonfoods.nutrients[0].value - ($scope.jsonfoods.nutrients[0].value * (5/100)) && input < $scope.jsonfoods.nutrients[0].value + ($scope.jsonfoods.nutrients[0].value * (5/100)) )
+          console.log(this.$scope.jsonfoods);
+          self.showProtein = value;
+        }
+      }
+    };
   }]);
 })();
 
-
-
-// app.controller("foodsController", function(){
-//   console.log("before getFoods is run");
-//     this.getFoods = function (){
-//       console.log("button clicked");
-//       $http.get("http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=M4zdaQiev4SOfYzye5gC3xhVTanoFD4uKXt1TNe8&nutrients=205&nutrients=204&nutrients=208&nutrients=269")
-//       .success(function(response){
-//         console.log(response);
-//       })
-//       .fail(function(){
-//         console.log("call has failed");
-//       });
-//     };
-// }); // end of controller
 
 // this.create = function(){
 //   this.foods.unshift({
