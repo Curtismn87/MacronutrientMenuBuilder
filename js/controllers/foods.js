3eaf642fa33ff602113e1d9f4bd83c649e6e2f2b
Matt Curtis <Curtismn87@yahoo.com>
2015-09-16 23:28:16 -0400

    changed json data type to int

--- js/controllers/foods.js
+++ js/controllers/foods.js
@@ -8,10 +8,10 @@
     $scope.jsonfoods = {};
     this.getFoods = function(){
         console.log("button clicked");
-        $http.get("http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=M4zdaQiev4SOfYzye5gC3xhVTanoFD4uKXt1TNe8&max=15&fg=13&nutrients=205&nutrients=204&nutrients=203")
+        $http.get("http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=M4zdaQiev4SOfYzye5gC3xhVTanoFD4uKXt1TNe8&max=1500&fg=13&nutrients=205&nutrients=204&nutrients=203")
         .success(function(response){
           $scope.jsonfoods = response.report.foods;
-          console.log(response.report.foods);
+          console.log("this is the resposne" + response.report.foods);
         });
       };
       this.delete = function(index){
@@ -22,18 +22,23 @@
       self.showProtein = true;
       self.toggleShowProtein = function(value){
         var input = this.protein;
+        console.log("this is the input" + input);
         console.log("this is all the foods" + $scope.jsonfoods);
         console.log("this is food at index 1" + $scope.jsonfoods[1].nutrients);
         for (var i=0; i < $scope.jsonfoods.length; i ++){
-            var protein = $scope.jsonfoods[i].nutrients[0].value;
+            var protein = parseFloat($scope.jsonfoods[i].nutrients[0].value);
             var fat = $scope.jsonfoods[i].nutrients[1].value;
             var carbs = $scope.jsonfoods[i].nutrients[2].value;
-            if (input > protein - (protein * (5/100)) && input < protein + (protein * (5/100))){
-              self.showProtein = value;
+            if (input >= protein - (protein * (5/100)) && input <= protein + (protein * (5/100))){
               console.log("passed" + $scope.jsonfoods[i].name + protein);
+              $("#"+i).show();
+              console.log("this is the input" + input);
             }
             else {
-              console.log("failed" + $scope.jsonfoods[i].name + protein);
+              // console.log("failed" + $scope.jsonfoods[i].name + protein);
+              // console.log($("#"+i));
+              // console.log("this is the input" + input);
+              $("#"+i).hide();
             }
         }
       };
