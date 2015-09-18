86431fa3776522959669cee1e62881af2982a71c
Matt Curtis <Curtismn87@yahoo.com>
2015-09-17 20:08:19 -0400

    added some styling

--- js/controllers/foods.js
+++ js/controllers/foods.js
@@ -5,6 +5,7 @@
     // var ref = new Firebase("https://macronutrientmenu.firebaseio.com");
     // $scope.data = $firebaseObject(ref);
     $scope.jsonfoods = {};
+    $menuItems = [];
     this.getFoods = function(){
       var options = $("select option:selected").val();
       var foodsURL = "http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=M4zdaQiev4SOfYzye5gC3xhVTanoFD4uKXt1TNe8&max=1500&fg="+ options +"&nutrients=205&nutrients=204&nutrients=203";
@@ -16,26 +17,38 @@
       this.delete = function(index){
         $scope.jsonfoods.splice(index, 1);
       };
+      this.add = function(index){
+        console.log($scope.jsonfoods.val());
+        // $menuItems.push($(this))
+      };
 
       var self = this;
       self.toggleShowProtein = function(value){
-        var input = this.protein;
+        var proteinInput = this.protein;
+        var fatInput = this.fat;
+        var carbInput = this.carbohydrate;
         for (var i=0; i < $scope.jsonfoods.length; i ++){
             var protein = parseFloat($scope.jsonfoods[i].nutrients[0].value);
-            var fat = $scope.jsonfoods[i].nutrients[1].value;
-            var carbs = $scope.jsonfoods[i].nutrients[2].value;
-            if (input >= protein - (protein * (5/100)) && input <= protein + (protein * (5/100))){
+            var fat = parseFloat($scope.jsonfoods[i].nutrients[1].value);
+            var carbs = parseFloat($scope.jsonfoods[i].nutrients[2].value);
+            if (proteinInput >= protein - (protein * (5/100)) && proteinInput <= protein + (protein * (5/100))){
               $("#"+i).show();
             }
             else {
               $("#"+i).hide();
             }
         }
-      };
 
+      };
 
   }]);
 })();
+$(".food").mouseover(function(){
+  $(".food").css("background-color", "black");
+});
+$(".food").on("click", function(){
+  console.log("div was clicked");
+});
 
 
 
