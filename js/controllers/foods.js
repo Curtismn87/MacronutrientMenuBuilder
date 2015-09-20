0e993626189b329cab2fc39166010fdd7f55827d
Matt Curtis <Curtismn87@yahoo.com>
2015-09-20 17:20:35 -0400

    Cleaned up code

--- js/controllers/foods.js
+++ js/controllers/foods.js
@@ -2,27 +2,20 @@
   var app = angular.module("MenuBuilder", []);
 
   app.controller("foodsController",["$scope", "$http", function($scope, $http){
-    // var ref = new Firebase("https://macronutrientmenu.firebaseio.com");
-    // $scope.data = $firebaseObject(ref);
     $scope.jsonfoods = {};
     $scope.addedFood = [];
 
     this.getFoods = function(){
-      console.log("button Clicked");
       var options = $("select option:selected").val();
       var foodsURL = "http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=M4zdaQiev4SOfYzye5gC3xhVTanoFD4uKXt1TNe8&max=1500&fg="+ options +"&nutrients=205&nutrients=204&nutrients=203";
         $http.get(foodsURL)
         .success(function(response){
           $scope.jsonfoods = response.report.foods;
-          console.log($scope.jsonfoods);
         });
       };
       this.add = function(index){
-        console.log($scope.addedFood);
         var newFood = $scope.jsonfoods.splice(index, 1);
         $scope.addedFood.push(newFood);
-        console.log($scope.addedFood);
-        console.log($scope.addedFood[0][0].name);
       };
       var self = this;
       self.toggleShowProtein = function(value){
@@ -47,22 +40,3 @@
 
   }]);
 })();
-
-
-
-// this.create = function(){
-//   this.foods.unshift({
-//     name: this.food,
-//   });
-//   this.reset();
-// }; // end of create function
-// this.reset = function(){
-//       this.food = "";
-//     }; // end reset function
-// this.edit = function(index){
-//   var food = this.foods[index];
-//   food.name = this.name;
-// };
-
-// };
-//
