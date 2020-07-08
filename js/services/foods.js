f7445f7a2399622263e7e96d0f4ea4916545e982
curtismn87 <curtismn87@yahoo.com>
2020-07-07 20:51:17 -0400

    Added another proxyurl

--- js/services/foods.js
+++ js/services/foods.js
@@ -1,6 +1,6 @@
 (function(){
   var foodServices = angular.module('foodServices', ["ngResource"]);
   foodServices.factory("MenuBuilder", ["$resource", function($resource){
-    return $resource("http://api.nal.usda.gov/ndb/list?format=json&lt=f&sort=n&api_key=M4zdaQiev4SOfYzye5gC3xhVTanoFD4uKXt1TNe8");
+    return $resource("https://cors-anywhere.herokuapp.com/http://api.nal.usda.gov/ndb/list?format=json&lt=f&sort=n&api_key=M4zdaQiev4SOfYzye5gC3xhVTanoFD4uKXt1TNe8");
   }]);
 });
