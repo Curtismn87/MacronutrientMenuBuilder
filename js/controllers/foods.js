c5ca9f96e1280e217fb72c20840efa204326c7a3
curtismn87 <curtismn87@yahoo.com>
2020-07-07 22:52:14 -0400

    Removed all CORS attempted

--- js/controllers/foods.js
+++ js/controllers/foods.js
@@ -8,11 +8,9 @@
 
     this.getFoods = function(){
       var options = $("select option:selected").val();
-      var proxyurl = "https://cors-anywhere.herokuapp.com/";
       var foodsURL = "http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=M4zdaQiev4SOfYzye5gC3xhVTanoFD4uKXt1TNe8&max=1500&fg="+ options +"&nutrients=205&nutrients=204&nutrients=203";
-        $http.get(proxyurl + foodsURL)
+        $http.get(foodsURL)
         .success(function(response){
-          response.header("Access-Control-Allow-Origin", "*");
           $scope.jsonfoods = response.report.foods;
         });
       };
