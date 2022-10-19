570b2270f57a6361024478ad211830fff91033e7
Matt Curtis <Curtismn87@yahoo.com>
2022-10-19 12:48:48 -0400

    Update API call, see if works.

--- js/controllers/foods.js
+++ js/controllers/foods.js
@@ -8,7 +8,9 @@
 
     this.getFoods = function(){
       var options = $("select option:selected").val();
-      var foodsURL = "http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=M4zdaQiev4SOfYzye5gC3xhVTanoFD4uKXt1TNe8&max=1500&fg="+ options +"&nutrients=205&nutrients=204&nutrients=203";
+//      var foodsURL = "http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key=M4zdaQiev4SOfYzye5gC3xhVTanoFD4uKXt1TNe8&max=1500&fg="+ options +"&nutrients=205&nutrients=204&nutrients=203";
+      var foodsURL = "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?limit=1&api_key=M4zdaQiev4SOfYzye5gC3xhVTanoFD4uKXt1TNe8";
+
         $http.get(foodsURL)
         .success(function(response){
           $scope.jsonfoods = response.report.foods;
